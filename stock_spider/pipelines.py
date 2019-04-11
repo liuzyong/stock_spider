c6a5de0c2b4c19c271bf9a54d3372549fe2d2396
lau <412374299@qq.com>
2019-04-11 20:54:28 +0800

    修改文件

--- stock_spider/pipelines.py
+++ stock_spider/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
 
 class StockSpiderPipeline(object):
     def process_item(self, item, spider):
@@ -18,4 +19,16 @@ class StockPipeline(object):
         return item
 
     def save_to_file(self, item):
-        pass
+        try:
+            filename = ".\\test.txt"
+            file = open(filename, mode='a+')
+            # 判断文件是否非空
+            if 0 == os.path.getsize(filename):
+                file.write("qqqqqq\n")
+
+            file.write(str(item) + "\n")
+            file.flush()
+            file.close()
+        except():
+            pass
+
